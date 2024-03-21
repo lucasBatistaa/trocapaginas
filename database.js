83d3b537b5f7e32e6d3809f87855148d4b67d6b3
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-21 09:55:18 -0300

    conectando a um servidor de banco de dados

--- database.js
+++ database.js
@@ -6,14 +6,14 @@ const client = new Client({
     user: process.env.DB_USER,
     port: process.env.DB_PORT,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
 })
 
 client.connect();
 
 client.query(`select * from users`, (err, res) => {
     if(!err) {
-        console.log(res.rows);
+        console.log('conectado');
     
     }else {
         console.log(err.message);
