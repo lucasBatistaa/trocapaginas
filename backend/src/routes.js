862beebf0b9e457e3c5d621cf1dfb73cb182a7eb
Maria Eduarda <mariaeduardadefaria15@gmail.com>
2024-03-24 19:59:26 -0300

    backend login concluído

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,8 +4,6 @@ import {Database} from '../database.js';
 const routes = express.Router();
 const database = new Database();
 
-let usersArray;
-
 database.getUsers().then(users => {
     routes.post('/login', (req, res) => {
         const {email, password} = req.body;
