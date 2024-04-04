30c1e167972fb46c3e60158871cd7e43bbf5257c
Stephanie Victoria <stephanievictoria.dossantos@gmail.com>
2024-04-04 19:23:32 -0300

    component passwordRequirements: validation & handleSubmit

--- frontend/src/components/PasswordRequirements/index.jsx
+++ frontend/src/components/PasswordRequirements/index.jsx
@@ -5,44 +5,111 @@ import Ionicons from '@expo/vector-icons/Ionicons';
 
 
 import Input from "../Input"
+import Button from "../Button";
+import IsFormEmpty from "../../utils/isFormEmpty";
+import { THEME } from "../../styles/Theme"; 
+import styles from "./styles"
 
 export default function PasswordRequirements (props) {
     const [password, setPassword] = useState("");
-    const [validate, setValidate] = useState(props.validate);
+    const [passwordError, setPasswordError] = useState(false)
+    const [confirmPassword, setConfirmPassword] = useState("")
+    const [confirmPasswordError, setConfirmPasswordError] = useState(false)
+    const [messageError, setMessageError] = useState("")
     const [ caracters, setCaracters ] = useState(false)
-    const [ simbols, setSimbols ] = useState(true)
-    const requirements = /^.{8,}$/
-    
-    
-    requirements.test(password)
-    // return
+    const [letterCase, setLetterCase] = useState(false)
+    const [numbers, setNumbers] = useState(false)
+    const [ simbols, setSimbols ] = useState(false)
+    const minimum = /^.{8,}$/
+    const hasNumber = /(?=.*\d)/
+    const hasLetterCase = /^(?=.*[a-z])(?=.*[A-Z]).+$/
+    const hasSimbol = /\W|_/
 
     function validPassword (password) {
-        setCaracters(requirements.test(password))
+        setCaracters(minimum.test(password))
+        setLetterCase(hasLetterCase.test(password))
+        setNumbers(hasNumber.test(password))
+        setSimbols(hasSimbol.test(password))
     }
+
+    const handleSubmit = () => {
+        const isEmptyPassword = IsFormEmpty(password) 
+        setPasswordError(isEmptyPassword)
+
+        const isEmptyConfirmPassword = IsFormEmpty(confirmPassword) 
+        setConfirmPasswordError(isEmptyConfirmPassword)
+
+        if (!isEmptyPassword && !isEmptyConfirmPassword) {
+            setMessageError("")
+
+            if (caracters && numbers && letterCase && simbols){
+
+                if (password == confirmPassword) {
+                    navigation.navigate('Slogan');
+                    //mandar para o banco
+                }
+                else {
+                    setMessageError("As senhas não conferem")
+                    setConfirmPasswordError(true)
+                }
+            }
+            else {
+                setMessageError("A senha precisa atender todos os requisitos.")
+                setPasswordError(true)
+            }
+        }
+        else {
+            setMessageError("insira todos os campos!")
+        }
+    } 
 
     return (
         <View>
             <Input
-            label={"Senha"}
-            placeholder={props.placeholder}
-            value={password}
-            onChangeText={(password) => {
-                setPassword(password)
-                validPassword(password)
-            }}
-            secureTextEntry
+                label={"Senha"}
+                placeholder={"Insira sua senha"}
+                value={password}
+                onChangeText={(password) => {
+                    setPassword(password)
+                    validPassword(password)
+                }}
+                style={passwordError && THEME.errors.input}
+                secureTextEntry
             />
 
-            {props.validate && (
+            {
                 <View>
                     {caracters ? <Ionicons name="checkmark-circle-outline" size={20} color={"#196805"}/> : <Ionicons name="close-circle-outline" size={20} color={"#CE1F1F"}/>}
-                    {/* <View>{simbols ? <Ionicons name="checkmark-circle-outline" size={20} color={"#196805"}/> : <Ionicons name="close-circle-outline" size={20} color={"#CE1F1F"}/>} <Text>Letras maiúsculas e minúsculas</Text></View> */}
-                
-                </View>
-                )}
-        </View>
-        
+                    <Text>Pelo menos oito caracteres</Text>
 
+                    {letterCase ? <Ionicons name="checkmark-circle-outline" size={20} color={"#196805"}/> : <Ionicons name="close-circle-outline" size={20} color={"#CE1F1F"}/>}
+                    <Text>Letras maiúsculas e minúsculas</Text>     
+
+                    {numbers ? <Ionicons name="checkmark-circle-outline" size={20} color={"#196805"}/> : <Ionicons name="close-circle-outline" size={20} color={"#CE1F1F"}/>}
+                    <Text>Pelo menos um número</Text>
+
+                    {simbols ? <Ionicons name="checkmark-circle-outline" size={20} color={"#196805"}/> : <Ionicons name="close-circle-outline" size={20} color={"#CE1F1F"}/>}
+                    <Text>Pelo menos um símbolo especial (Ex.: @ # $ ! *)</Text>
+                </View>
+            }
+
+            <Input
+                label={"Confirmr senha"}
+                placeholder={"Confirme sua senha"}
+                value={confirmPassword}
+                onChangeText={setConfirmPassword}
+                style={confirmPasswordError && THEME.errors.input}
+                secureTextEntry
+            />
+
+            {messageError && <Text style={[THEME.fonts.text, THEME.errors.message]}>{messageError}</Text>}
+
+            <Button
+                onPress={handleSubmit}
+                title={"ALTERAR"}
+                color={'brownDark'}
+            />
+
+        </View>
     )
 }
--- frontend/src/pages/Reset/confirmUser.jsx
+++ frontend/src/pages/Reset/confirmUser.jsx
@@ -1,26 +1,14 @@
-import { useEffect, useState } from "react";
-import { Text, View, TextInput, TouchableOpacity } from "react-native";
-import axios from "axios";
+import { Text, View } from "react-native";
 
-import Button from "../../components/Button";
-
-import { styles } from "./styles";
-import Input from "../../components/Input";
-import { THEME } from "../../styles/Theme";
-import { useNavigation } from "@react-navigation/native";
 import PasswordRequirements from "../../components/PasswordRequirements";
 
 export default function Reset () {
-    const [email, setEmail] = useState('')
 
     return (
         <View>
             <Text>ALTERAR SENHA</Text>
 
-            <PasswordRequirements
-                placeholder={"Insira sua senha"}
-                validate
-            />
+            <PasswordRequirements/>
 
         </View>
     )
