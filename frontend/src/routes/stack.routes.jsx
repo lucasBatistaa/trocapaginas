bdecc88a702e028d2cc555afb5f90a5e715fef06
lucasbatistaa <lucas.lucas2004.30@gmail.com>
2024-04-17 21:20:45 -0300

    init page CreatePost

--- frontend/src/routes/stack.routes.jsx
+++ frontend/src/routes/stack.routes.jsx
@@ -6,18 +6,20 @@ import Register from '../pages/Register'
 import RegisterEmail from '../pages/Register/RegisterEmail'
 import Reset from '../pages/Reset'
 import InitialPage from '../pages/InitialPage'
+import CreatePost from '../pages/CreatePost'
 
 const Stack = createNativeStackNavigator()
 
 export default function StackRoutes() {
     return (
-        <Stack.Navigator initialRouteName='Slogan' screenOptions={{ headerShown: false}}>
+        <Stack.Navigator initialRouteName='CreatePost' screenOptions={{ headerShown: false}}>
             <Stack.Screen name='Slogan' component={Slogan} />
             <Stack.Screen name='Login' component={Login} />
             <Stack.Screen name='Register' component={Register} />
             <Stack.Screen name='RegisterEmail' component={RegisterEmail} />
             <Stack.Screen name='Reset' component={Reset}/>
             <Stack.Screen name='InitialPage' component={InitialPage}/>
+            <Stack.Screen name='CreatePost' component={CreatePost}/>
         </Stack.Navigator>
     )
 }
