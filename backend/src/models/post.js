944a49aaa512cab3a17b7253b706885de38f3b9d
Tulio Koogi <tulio-koogi@hotmail.com>
2024-04-28 01:33:32 -0300

    Alteração dos atributos da classe Post

--- backend/src/models/post.js
+++ backend/src/models/post.js
@@ -1,16 +1,15 @@
+import { ImageURI } from '../../../frontend/src/utils/imageURI.jsx';
 import {Database} from '../../database.js'
 
 const database = new Database();
 
 export class Post {
-    constructor(idPost, idUser, content, timePost, likes, titleReview, textReview, image) {
+    constructor(idPost, idUser, content, timePost, image) {
         this.idPost = idPost;
         this.idUser = idUser;
         this.content = content;
         this.timePost = timePost;
-        this.likes = likes;
-        this.titleReview = titleReview;
-        this.textReview = textReview;
-        this.image= image;
+        this.nameBook = nameBook;
+        this.imageUri= imageUri;
     }
  }
