0a8e5f37cf6f1c90effe4e8b270d742fe0975dc9
Stephanie Victoria <stephanievictoria.dossantos@gmail.com>
2024-05-15 21:47:37 -0300

    finish LateralMenu styles

--- frontend/src/components/Menus/LateralMenu/index.jsx
+++ frontend/src/components/Menus/LateralMenu/index.jsx
@@ -2,7 +2,10 @@ import { View, Modal, Text, TouchableOpacity, Image } from "react-native"
 
 import EditIcon from '../../../assets/edit-icon.svg'
 import ExitIcon from '../../../assets/exit-icon.svg'
+import Logo from '../../../assets/logo.svg'
+
 import { styles } from "./style"
+import { THEME } from "../../../styles/Theme"
 
 export default function LateralMenu ({menuVisible, onPress}) {
     return(
@@ -14,25 +17,24 @@ export default function LateralMenu ({menuVisible, onPress}) {
         
         <View style={styles.principalView}>   
             <TouchableOpacity
-                //style={styles.clickClose}
+                style={styles.clickClose}
                 onPress={onPress}
             >
             </TouchableOpacity>
 
-            <View>
-                <Image
-                    style={{width: 124, height: 110}}
-                    source={require('../../../assets/logo.png')}
+            <View style={styles.menuContainer}>
+                <Logo
+                    style={{width: 140, height: 140, alignSelf: 'center'}}
                 />
 
-                <View>
+                <View style={[styles.viewOption,{marginBottom: 28}]}>
                     <EditIcon/>
-                    <Text>Editar Perfil</Text>
+                    <Text style={[THEME.fonts.h1.normal, {color: THEME.colors.brownDark}]}>Editar Perfil</Text>
                 </View>
 
-                <View>
+                <View style={styles.viewOption}>
                     <ExitIcon/>
-                    <Text>Sair</Text>
+                    <Text style={[THEME.fonts.h1.normal, {color: THEME.colors.red}]}>Sair</Text>
                 </View>
             </View>
         </View>
